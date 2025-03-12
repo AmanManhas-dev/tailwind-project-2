e7eac40eca5821b75eb0856314ad32c2f5e7637b
AmanManhas-dev <amanmanhas0987123@gmail.com>
2025-03-12 20:08:18 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+let colour='red'
+if (colour==='red'){
+   
+console.log('stop');
+}
+if (colour==='yellow'){
+   
+    console.log('wait');
+    }
+    if (colour==='green'){
+   
+        console.log('go');
+        }
+            
